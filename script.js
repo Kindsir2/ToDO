f743bc6827f3016c28917db6fb32b530234f94c9
kindsir2 <jashautar3116@gmail.com>
2024-11-18 10:38:11 -0300

    new functions

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let taskList = [];
 
 function addTask() {
-  let newTaskDOm = document.querySelector(".bottom-input");
   let newTask = document.querySelector(".input").value;
+  let newTaskDOm = document.querySelector(".bottom-input");
   let taskHtml = ""
   let addNewTask = taskList.push(newTask);
 
@@ -19,7 +19,7 @@ function addTask() {
         
         <div class="task-thing task-text">${taskList} </div> 
         
-        <div class="task-thing delete">X</div>
+        <div class="task-thing delete" onclick="deleteTask">X</div>
       </div>
   `
   
@@ -27,11 +27,42 @@ function addTask() {
     
   })
   
-}
+  console.log(taskList);
   
+}
+
+
+
+
+
+function clickEnter() {
+  let input = document.querySelector(".input")
+  
+  input.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      document.querySelector(".add").click();
+    }
+  });
+  console.log("balls");
+}
+
+
+
+clickEnter();
+  
+
+
+  
+function noTaskWarning(taskList) {
+  const empty = "";
+  if(taskList == empty) {
+    alert("add task")
+  }
+}
+
+noTaskWarning();
+
+
+
 addTask();
-  
-  
-function deleteTask() {
-  
-}
