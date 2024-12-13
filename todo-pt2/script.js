e9cdf98f5b11e4f552d02ccef549ec3d0145b230
kindsir2 <jashautar3116@gmail.com>
2024-12-13 11:36:43 -0300

    finish

--- todo-pt2/script.js
+++ todo-pt2/script.js
@@ -1,46 +1,56 @@
-let taskList = [];
-
+let taskList = []; 
 let input = document.querySelector(".input"); 
 
-document.querySelector(".add-button").addEventListener("click", () => {
-   
-  const date = new Date;
+function addTask() {
+  const date = new Date();
 
-  const newTask = 
-   {
-     check : false, 
-     time: date.getSeconds() , 
-     task: input.value,
-     date: document.querySelector(".calender").value
-   };
-   
- taskList.push(newTask)
+  const newTask = {
+    check: false,
+    id: date.getTime(),
+    task: input.value,
+    date: document.querySelector(".calender").value,
+  };
+
+    taskList.push(newTask);
+    input.value = "";
+    renderTasks();
   
- console.log(newTask.date);
- 
- 
- let taskHtml = "";
- 
- taskList.forEach((taskList) => {
-   
-  console.log(taskList.task);  
+}
 
-  taskHtml +=
-  `
-  <label class="task-text">
-                  ${taskList.task}
-                   <input type="checkbox">
-            <span class="checkmark"></span>
-            <button class="delete">&#x2716;</button>
-            <div>${taskList.date}</div>
-          </label>
+function renderTasks() {
+  const taskDiv = document.querySelector(".task-div");
+  taskDiv.innerHTML = "";
 
-          
-  `
-    
-  document.querySelector(".task-div").innerHTML=taskHtml;
-  })
-})
+  taskList.forEach((task) => {
+    const taskHtml = `
+      <label class="task-text" data-id="${task.id}">
+        ${task.task}
+        <input type="checkbox">
+        <span class="checkmark"></span>
+        <button class="delete" data-id="${task.id}">&#x2716;</button>
+        <div>${task.date}</div>
+      </label>
+    `;
+    taskDiv.innerHTML += taskHtml;
+  });
+
+  const deleteButtons = document.querySelectorAll(".task-div");
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const taskId = parseInt(event.target.getAttribute("data-id"), 10);
+      deleteTask(taskId);
+    });
+  });
+}
+
+function deleteTask(taskId) {
+  taskList = taskList.filter((task) => task.id !== taskId);
+  renderTasks();
+}
+
+document.querySelector(".add-button").addEventListener("click", addTask);
+
+console.table(taskList);
 
 
 /* create html in js
@@ -49,3 +59,6 @@ document.querySelector(".add-button").addEventListener("click", () => {
  p.textContent = "banana";
  document.body.appendChild(p);
 */
+
+
+//It’s because your console is only executed once when the page loads. Which is when the array is empty. After that when the event listener is fired the array is populated. So the console which you have declared outside is not executed anymore. Just remember the program follows a linear flow.Some thing/like a user action needs to happen before your logic can be executed. That’s why when you have a console log in the event listener callback, you will see it’s output.
